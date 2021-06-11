b64fb083d780626da3220f91c1a0503fc2806894
Àre <arematurana@hotmail.com>
2021-06-11 22:11:11 +0200

    main menu added

--- GMTKGameJamProject/Assets/Scripts/mainMenu.cs
+++ GMTKGameJamProject/Assets/Scripts/mainMenu.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void startButton()
     {
-        
+        Debug.Log("Start");
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void quitButton()
     {
-        
+        Application.Quit();
     }
+
 }
