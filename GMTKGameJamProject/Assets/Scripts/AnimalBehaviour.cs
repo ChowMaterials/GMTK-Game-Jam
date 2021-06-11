965126fc78151e3c52ceb1ba931f164390c2c707
ChowMaterials <85402354+ChowMaterials@users.noreply.github.com>
2021-06-11 23:43:39 +0100

    Game

--- GMTKGameJamProject/Assets/Scripts/AnimalBehaviour.cs
+++ GMTKGameJamProject/Assets/Scripts/AnimalBehaviour.cs
@@ -5,36 +5,69 @@ using UnityEngine;
 public class AnimalBehaviour : MonoBehaviour
 {
     public static Vector3 DesiredPosition;
+    private int Health;
+    private float AttackCooldown;
+    [SerializeField] float AttackSpeed;
+    [SerializeField] float MovementSpeed;
+
     void Start()
     {
+        Health = 100;
         DesiredPosition = new Vector3(0,0,0);
+        AttackCooldown = 0;
     }
 
     
     void Update()
     {
         Move();
+        AttackCooldown += Time.deltaTime;
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Attack(other);
+        }
+
+    }
+
 
     void Move()
     {
         var _DistanceToTarget = Vector3.Distance(transform.position, DesiredPosition);
         if (_DistanceToTarget>2)
         {
-            transform.position = Vector3.Lerp(transform.position, DesiredPosition, Time.deltaTime *0.1f);
+            transform.position = Vector3.Lerp(transform.position, DesiredPosition, Time.deltaTime *MovementSpeed);
 
+        }
+    }
+
+
+
+    void Attack(Collider2D _Enemy)
+    {
+        if(AttackCooldown>AttackSpeed)
+        {
+            _Enemy.gameObject.GetComponent<enemySpawn>();
+
+            //Deal damage to the humanHere
         }
 
 
     }
 
-
-
-    void Attack()
+    public void TakeDamage(int _Damage)
     {
-
-
-
+        Health -= _Damage;
+        if(Health<0)
+        {
+            Death();
+        }
     }
-
+    void Death()
+    {
+        Debug.Log("Dead");
+    }
 }
